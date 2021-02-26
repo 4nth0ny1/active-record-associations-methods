e73aabfe2571ba607e4865ea008de43f1df27dbd
4anth0ny1 <acatullo4@gmail.com>
2021-02-26 16:03:16 -0600

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,12 +3,31 @@ class Song < ActiveRecord::Base
   belongs_to :genre
 
   def get_genre_name
+    self.genre.name
   end
 
   def drake_made_this
+    if Artist.where(name: "Drake")
+        drake = Artist.find_by(name: "Drake")
+        drake.songs << @song
+        @song.artist
+    else 
+      Artist.create(name: "Drake")
+      self
+    end     
+  end
+end
+
     # when this method is called it should assign the song's artist to Drake
     # Drake doesn't exist in the database as an artist yet, so you'll have to create a record
     # Hint: you won't want to create an artist record every time this method is called, only if an Drake is *not found*
+    
+    ## create conditional for if drake exists. if he does then assign the song's artist to drake. 
+    ## we are given a song, check if drake exits, then if so, assign drake as the artist of the song. 
+    # forever = Song.create(name: "Forever")
+    # drake = Artist.create(name: "Drake")
+    # forever.artist = drake
 
-  end
-end
+    # @song = Song.create(name: "Forever")
+    # drake.songs << @song
+    # forever.artist
